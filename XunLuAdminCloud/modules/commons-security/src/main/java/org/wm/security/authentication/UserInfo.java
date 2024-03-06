db97b369bdc22f6479fc53310efbdc8eb3d4ab33
KillerTwo <lwt2710@163.com>
2024-03-06 23:07:33 +0800

    用户认证功能调整

---
+++ XunLuAdminCloud/modules/commons-security/src/main/java/org/wm/security/authentication/UserInfo.java
@@ -0,0 +1,11 @@
+package org.wm.security.authentication;
+
+/**
+ * 功能描述：<功能描述>
+ *
+ * @author dove 
+ * @date 2024/03/06 19:37
+ * @since 1.0
+**/
+public class UserInfo {
+}
---
+++ XunLuAdminCloud/modules/commons-security/src/main/java/org/wm/security/authentication/opequeToken/CustomOpequeTokenAuthentication.java
@@ -0,0 +1,11 @@
+package org.wm.security.authentication.opequeToken;
+
+/**
+ * 功能描述：<功能描述>
+ *
+ * @author dove 
+ * @date 2024/03/06 22:56
+ * @since 1.0
+**/
+public class CustomOpequeTokenAuthentication {
+}
