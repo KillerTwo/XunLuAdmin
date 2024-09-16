bc8489fa1309cf85dd69ffc906b0127d156cddb0
KillerTwo <lwt2710@163.com>
2024-09-16 23:48:27 +0800

    addui

--- generator-ui/apps/web-antd/src/views/gen/code/index.vue
+++ generator-ui/apps/web-antd/src/views/gen/code/index.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 // import { SmileOutlined, DownOutlined } from '@vben/icons';
-import type { DrawerProps, TableProps } from 'ant-design-vue';
+import type { DrawerProps, SelectProps, TableProps } from 'ant-design-vue';
 
 import { computed, reactive, ref, watch, watchEffect } from 'vue';
 import type { Ref, UnwrapRef } from 'vue';
@@ -36,7 +36,7 @@ interface ColumnType {
   htmlType: string;
   isIncrement: string;
   isPk: string;
-  isRequired: string;
+  isRequired: boolean;
   javaField: string;
   javaType: string;
   sort: number;
@@ -74,130 +74,144 @@ const editColumns = [
   {
     dataIndex: 'columnId',
     key: 'columnId',
+    minWidth: 100,
     title: '编号',
+    width: 100,
   },
   {
     dataIndex: 'columnName',
     key: 'columnName',
     title: '字段名',
+    width: 100,
   },
   {
     dataIndex: 'columnComment',
     key: 'columnComment',
     title: '字段说明',
+    width: 100,
   },
   {
     dataIndex: 'columnType',
     key: 'columnType',
     title: '字段类型',
+    width: 100,
   },
   {
     dataIndex: 'javaField',
     key: 'javaField',
     title: '属性名',
+    width: 100,
   },
   {
     dataIndex: 'javaType',
     key: 'javaType',
     title: '属性类型',
+    width: 100,
   },
   {
     dataIndex: 'typeScriptType',
     key: 'typeScriptType',
     title: 'TypeScript类型',
+    width: 100,
   },
   {
     dataIndex: 'isPk',
     key: 'isPk',
     title: '是否主键',
+    width: 100,
   },
   {
     dataIndex: 'isIncrement',
     key: 'isIncrement',
     title: '是否自增',
+    width: 100,
   },
   {
     dataIndex: 'isRequired',
     key: 'isRequired',
     title: '是否必填',
+    width: 100,
   },
   {
     dataIndex: 'htmlType',
     key: 'htmlType',
     title: '显示类型',
+    width: 100,
   },
   {
     dataIndex: 'dictType',
     key: 'dictType',
     title: '字典类型',
+    width: 100,
   },
   {
     dataIndex: 'sort',
     key: 'sort',
     title: '排序',
+    width: 100,
   },
 ];
 
 const dataSource: Ref<ColumnType[]> = ref([
   {
-    columnComment: 'London',
+    columnComment: '用户名',
     columnId: 1,
-    columnName: 'Edward King 0',
-    columnType: 'London, Park Lane no. 0',
-    dictType: 'London, Park Lane no. 0',
-    htmlType: 'London, Park Lane no. 0',
-    isIncrement: 'London, Park Lane no. 0',
-    isPk: 'London, Park Lane no. 0',
-    isRequired: 'London, Park Lane no. 0',
-    javaField: 'London, Park Lane no. 0',
-    javaType: 'London, Park Lane no. 0',
+    columnName: 'username',
+    columnType: 'VARCHAR(20)',
+    dictType: '1',
+    htmlType: 'input',
+    isIncrement: '否',
+    isPk: '否',
+    isRequired: true,
+    javaField: 'username',
+    javaType: 'String',
     sort: 1,
-    typeScriptType: 'London, Park Lane no. 0',
+    typeScriptType: 'string',
   },
   {
-    columnComment: 'London',
+    columnComment: '密码',
     columnId: 2,
-    columnName: 'Edward King 0',
-    columnType: 'London, Park Lane no. 0',
-    dictType: 'London, Park Lane no. 0',
-    htmlType: 'London, Park Lane no. 0',
-    isIncrement: 'London, Park Lane no. 0',
-    isPk: 'London, Park Lane no. 0',
-    isRequired: 'London, Park Lane no. 0',
-    javaField: 'London, Park Lane no. 0',
-    javaType: 'London, Park Lane no. 0',
+    columnName: 'password',
+    columnType: 'VARCHAR(20)',
+    dictType: '1',
+    htmlType: 'input',
+    isIncrement: '否',
+    isPk: '否',
+    isRequired: true,
+    javaField: 'password',
+    javaType: 'String',
     sort: 2,
-    typeScriptType: 'London, Park Lane no. 0',
+    typeScriptType: 'string',
   },
   {
-    columnComment: 'London',
+    columnComment: '性别',
     columnId: 3,
-    columnName: 'Edward King 0',
-    columnType: 'London, Park Lane no. 0',
-    dictType: 'London, Park Lane no. 0',
-    htmlType: 'London, Park Lane no. 0',
-    isIncrement: 'London, Park Lane no. 0',
-    isPk: 'London, Park Lane no. 0',
-    isRequired: 'London, Park Lane no. 0',
-    javaField: 'London, Park Lane no. 0',
-    javaType: 'London, Park Lane no. 0',
+    columnName: 'sex',
+    columnType: 'VARCHAR(20)',
+    dictType: '1',
+    htmlType: 'input',
+    isIncrement: '否',
+    isPk: '否',
+    isRequired: false,
+    javaField: 'sex',
+    javaType: 'String',
     sort: 3,
-    typeScriptType: 'London, Park Lane no. 0',
+    typeScriptType: 'string',
   },
   {
-    columnComment: 'London',
+    columnComment: '年龄',
     columnId: 4,
-    columnName: 'Edward King 0',
-    columnType: 'London, Park Lane no. 0',
-    dictType: 'London, Park Lane no. 0',
-    htmlType: 'London, Park Lane no. 0',
-    isIncrement: 'London, Park Lane no. 0',
-    isPk: 'London, Park Lane no. 0',
-    isRequired: 'London, Park Lane no. 0',
-    javaField: 'London, Park Lane no. 0',
-    javaType: 'London, Park Lane no. 0',
+    columnName: 'age',
+    columnType: 'VARCHAR(20)',
+    dictType: '1',
+    htmlType: 'input',
+    isIncrement: '否',
+    isPk: '否',
+    isRequired: false,
+    javaField: 'age',
+    javaType: 'String',
     sort: 4,
-    typeScriptType: 'London, Park Lane no. 0',
+    typeScriptType: 'string',
   },
 ]);
 
@@ -286,19 +300,69 @@ const save = (columnId: number, key: string) => {
 const handleJavaTypeChange = (value: string) => {
   console.log(`selected ${value}`);
 };
-const javaTypeOptions = [
-  { Integer: 'Integer' },
-  { Float: 'Float' },
-  { Long: 'Long' },
-  { Boolean: 'Boolean' },
-  { String: 'String' },
-  { Double: 'Double' },
-  { Date: 'Date' },
-  { LocalDate: 'LocalDate' },
-  { LocalTime: 'LocalTime' },
-  { LocalDateTime: 'LocalDateTime' },
-  { Object: 'Object' },
-];
+
+const handleJavascriptTypeChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
+
+const handleHtmlTypeChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
+
+const handleBlur = () => {
+  console.log('blur');
+};
+const handleFocus = () => {
+  console.log('focus');
+};
+
+const javaTypeOptions = ref<SelectProps['options']>([
+  { label: 'Integer', value: 'Integer' },
+  { label: 'Float', value: 'Float' },
+  { label: 'Long', value: 'Long' },
+  { label: 'Boolean', value: 'Boolean' },
+  { label: 'String', value: 'String' },
+  { label: 'Double', value: 'Double' },
+  { label: 'Date', value: 'Date' },
+  { label: 'LocalDate', value: 'LocalDate' },
+  { label: 'LocalTime', value: 'LocalTime' },
+  { label: 'LocalDateTime', value: 'LocalDateTime' },
+  { label: 'Object', value: 'Object' },
+]);
+
+const javaScriptTypeOptions = ref<SelectProps['options']>([
+  { label: 'Integer', value: 'Integer' },
+  { label: 'Float', value: 'Float' },
+  { label: 'Long', value: 'Long' },
+  { label: 'Boolean', value: 'Boolean' },
+  { label: 'String', value: 'String' },
+  { label: 'Double', value: 'Double' },
+  { label: 'Date', value: 'Date' },
+  { label: 'LocalDate', value: 'LocalDate' },
+  { label: 'LocalTime', value: 'LocalTime' },
+  { label: 'LocalDateTime', value: 'LocalDateTime' },
+  { label: 'Object', value: 'Object' },
+]);
+
+const htmlTypeOptions = ref<SelectProps['options']>([
+  { label: '文本框', value: 'input' },
+  { label: '文本域', value: 'textarea' },
+  { label: '下拉框', value: 'select' },
+  { label: '单选框', value: 'radio' },
+  { label: '复选框', value: 'checkbox' },
+  { label: '日期控件', value: 'datetime' },
+  { label: '图片上传', value: 'image' },
+  { label: '文本上传', value: 'file' },
+  { label: '富文本控件', value: 'richtextarea' },
+]);
+
+const htmlTypeFilter = (value: string) => {
+
+}
+
+const filterOption = (input: string, option: any) => {
+  return option.value.toLowerCase().includes(input.toLowerCase());
+};
 </script>
 
 <template>
@@ -363,7 +427,12 @@ const javaTypeOptions = [
         <a-button style="margin-right: 8px" @click="onClose">取消</a-button>
         <a-button type="primary" @click="onClose">确定</a-button>
       </template>
-      <a-table :columns="editColumns" :data-source="dataSource" bordered>
+      <a-table
+        :columns="editColumns"
+        :data-source="dataSource"
+        bordered
+        size="small"
+      >
         <template #bodyCell="{ column, text, record }">
           <template v-if="column.key === 'columnComment'">
             <div class="editable-cell">
@@ -371,20 +440,26 @@ const javaTypeOptions = [
                 v-if="editableData[`${record.columnId}_columnComment`]"
                 class="editable-cell-input-wrapper"
               >
-                <a-input
-                  v-model:value="
-                    editableData[`${record.columnId}_columnComment`]
-                      .columnComment
-                  "
-                  @press-enter="save(record.columnId, 'columnComment')"
-                />
-                <MaterialCheck
-                  @click="save(record.columnId, 'columnComment')"
-                />
+                <a-space style="margin-bottom: 10px">
+                  <a-input
+                    v-model:value="
+                      editableData[`${record.columnId}_columnComment`]
+                        .columnComment
+                    "
+                    @press-enter="save(record.columnId, 'columnComment')"
+                  />
+                  <MaterialCheck
+                    @click="save(record.columnId, 'columnComment')"
+                  />
+                </a-space>
               </div>
               <div v-else class="editable-cell-text-wrapper">
-                {{ text || ' ' }}
-                <MaterialEdit @click="edit(record.columnId, 'columnComment')" />
+                <a-space style="margin-bottom: 10px">
+                  {{ text || ' ' }}
+                  <MaterialEdit
+                    @click="edit(record.columnId, 'columnComment')"
+                  />
+                </a-space>
               </div>
             </div>
           </template>
@@ -394,17 +469,21 @@ const javaTypeOptions = [
                 v-if="editableData[`${record.columnId}_javaField`]"
                 class="editable-cell-input-wrapper"
               >
-                <a-input
-                  v-model:value="
-                    editableData[`${record.columnId}_javaField`].javaField
-                  "
-                  @press-enter="save(record.columnId, 'javaField')"
-                />
-                <MaterialCheck @click="save(record.columnId, 'javaField')" />
+                <a-space style="margin-bottom: 10px">
+                  <a-input
+                    v-model:value="
+                      editableData[`${record.columnId}_javaField`].javaField
+                    "
+                    @press-enter="save(record.columnId, 'javaField')"
+                  />
+                  <MaterialCheck @click="save(record.columnId, 'javaField')" />
+                </a-space>
               </div>
               <div v-else class="editable-cell-text-wrapper">
-                {{ text || ' ' }}
-                <MaterialEdit @click="edit(record.columnId, 'javaField')" />
+                <a-space style="margin-bottom: 10px">
+                  {{ text || ' ' }}
+                  <MaterialEdit @click="edit(record.columnId, 'javaField')" />
+                </a-space>
               </div>
             </div>
           </template>
@@ -414,22 +493,29 @@ const javaTypeOptions = [
                 v-if="editableData[`${record.columnId}_javaType`]"
                 class="editable-cell-input-wrapper"
               >
-                <a-select
-                  v-model:value="
-                    editableData[`${record.columnId}_javaType`].javaType
-                  "
-                  :options="javaTypeOptions"
-                  mode="tags"
-                  placeholder="Java属性类型"
-                  style="width: 100%"
-                  @change="handleJavaTypeChange"
-                  @press-enter="save(record.columnId, 'javaType')"
-                />
-                <MaterialCheck @click="save(record.columnId, 'javaType')" />
+                <a-space style="margin-bottom: 10px">
+                  <a-select
+                    v-model:value="
+                      editableData[`${record.columnId}_javaType`].javaType
+                    "
+                    :filter-option="filterOption"
+                    :options="javaTypeOptions"
+                    placeholder="选择java类型"
+                    show-search
+                    style="width: 100px"
+                    @blur="handleBlur"
+                    @change="handleJavaTypeChange"
+                    @focus="handleFocus"
+                    @press-enter="save(record.columnId, 'javaType')"
+                  />
+                  <MaterialCheck @click="save(record.columnId, 'javaType')" />
+                </a-space>
               </div>
               <div v-else class="editable-cell-text-wrapper">
-                {{ text || ' ' }}
-                <MaterialEdit @click="edit(record.columnId, 'javaType')" />
+                <a-space style="margin-bottom: 10px">
+                  {{ text || ' ' }}
+                  <MaterialEdit @click="edit(record.columnId, 'javaType')" />
+                </a-space>
               </div>
             </div>
           </template>
@@ -439,22 +525,55 @@ const javaTypeOptions = [
                 v-if="editableData[`${record.columnId}_typeScriptType`]"
                 class="editable-cell-input-wrapper"
               >
-                <a-input
-                  v-model:value="
-                    editableData[`${record.columnId}_typeScriptType`]
-                      .typeScriptType
-                  "
-                  @press-enter="save(record.columnId, 'typeScriptType')"
-                />
-                <MaterialCheck
-                  @click="save(record.columnId, 'typeScriptType')"
-                />
+                <a-space style="margin-bottom: 10px">
+                  <a-select
+                    v-model:value="
+                      editableData[`${record.columnId}_typeScriptType`]
+                        .typeScriptType
+                    "
+                    :filter-option="filterOption"
+                    :options="javaScriptTypeOptions"
+                    placeholder="选择javascript类型"
+                    show-search
+                    style="width: 100px"
+                    @change="handleJavascriptTypeChange"
+                    @press-enter="save(record.columnId, 'typeScriptType')"
+                  />
+                  <MaterialCheck
+                    @click="save(record.columnId, 'typeScriptType')"
+                  />
+                </a-space>
               </div>
               <div v-else class="editable-cell-text-wrapper">
-                {{ text || ' ' }}
-                <MaterialEdit
-                  @click="edit(record.columnId, 'typeScriptType')"
-                />
+                <a-space style="margin-bottom: 10px">
+                  {{ text || ' ' }}
+                  <MaterialEdit
+                    @click="edit(record.columnId, 'typeScriptType')"
+                  />
+                </a-space>
+              </div>
+            </div>
+          </template>
+          <template v-else-if="column.key === 'isRequired'">
+            <div class="editable-cell">
+              <div
+                v-if="editableData[`${record.columnId}_isRequired`]"
+                class="editable-cell-input-wrapper"
+              >
+                <a-space style="margin-bottom: 10px">
+                  <a-checkbox
+                    v-model:checked="
+                      editableData[`${record.columnId}_isRequired`].isRequired
+                    "
+                  />
+                  <MaterialCheck @click="save(record.columnId, 'isRequired')" />
+                </a-space>
+              </div>
+              <div v-else class="editable-cell-text-wrapper">
+                <a-space style="margin-bottom: 10px">
+                  {{ text ? '是' : '否' }}
+                  <MaterialEdit @click="edit(record.columnId, 'isRequired')" />
+                </a-space>
               </div>
             </div>
           </template>
@@ -464,17 +583,27 @@ const javaTypeOptions = [
                 v-if="editableData[`${record.columnId}_htmlType`]"
                 class="editable-cell-input-wrapper"
               >
-                <a-input
-                  v-model:value="
-                    editableData[`${record.columnId}_htmlType`].htmlType
-                  "
-                  @press-enter="save(record.columnId, 'htmlType')"
-                />
-                <MaterialCheck @click="save(record.columnId, 'htmlType')" />
+                <a-space style="margin-bottom: 10px">
+                  <a-select
+                    v-model:value="
+                      editableData[`${record.columnId}_htmlType`].htmlType
+                    "
+                    :filter-option="filterOption"
+                    :options="htmlTypeOptions"
+                    placeholder="选择显示类型"
+                    show-search
+                    style="width: 100px"
+                    @change="handleHtmlTypeChange"
+                    @press-enter="save(record.columnId, 'htmlType')"
+                  />
+                  <MaterialCheck @click="save(record.columnId, 'htmlType')" />
+                </a-space>
               </div>
               <div v-else class="editable-cell-text-wrapper">
-                {{ text || ' ' }}
-                <MaterialEdit @click="edit(record.columnId, 'htmlType')" />
+                <a-space style="margin-bottom: 10px">
+                  {{ text || ' ' }}
+                  <MaterialEdit @click="edit(record.columnId, 'htmlType')" />
+                </a-space>
               </div>
             </div>
           </template>
@@ -484,17 +613,21 @@ const javaTypeOptions = [
                 v-if="editableData[`${record.columnId}_dictType`]"
                 class="editable-cell-input-wrapper"
               >
-                <a-input
-                  v-model:value="
-                    editableData[`${record.columnId}_dictType`].dictType
-                  "
-                  @press-enter="save(record.columnId, 'dictType')"
-                />
-                <MaterialCheck @click="save(record.columnId, 'dictType')" />
+                <a-space style="margin-bottom: 10px">
+                  <a-input
+                    v-model:value="
+                      editableData[`${record.columnId}_dictType`].dictType
+                    "
+                    @press-enter="save(record.columnId, 'dictType')"
+                  />
+                  <MaterialCheck @click="save(record.columnId, 'dictType')" />
+                </a-space>
               </div>
               <div v-else class="editable-cell-text-wrapper">
-                {{ text || ' ' }}
-                <MaterialEdit @click="edit(record.columnId, 'dictType')" />
+                <a-space style="margin-bottom: 10px">
+                  {{ text || ' ' }}
+                  <MaterialEdit @click="edit(record.columnId, 'dictType')" />
+                </a-space>
               </div>
             </div>
           </template>
